729a4ca42d1b2cd250abe5212ab8d48cfb98e054
Allison Lowe <alliisonlowe@gmail.com>
2024-02-28 18:45:20 -0800

    Tkinter Testing Functions

--- pictureTolength.py
+++ pictureTolength.py
@@ -5,4 +5,13 @@ root = tk.Tk()
 root.geometry("800x500")
 root.title("LBCC ImagePixel-Length Software")
 
+label=tk.Label(root, text="Hello World!", font=('Arial', 18))
+label.pack(padx=20, pady=30)
+
+textbox =tk.Text(root,height=3, font=('Arial', 16))
+textbox.pack()
+
+myentry =tk.Entry(root)
+myentry.pack()
+
 root.mainloop()
