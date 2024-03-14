6ef814998f89f1a2a2f3aa133eba7a6f62090ae9
Allison Lowe <alliisonlowe@gmail.com>
2024-03-13 22:23:54 -0700

    Adding conversion functions

---
+++ App.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import math
+
+App = tk.Tk()
+App.title("LBCC Photogrammetry Software")
+App.geometry("1280x960")
+App.configure(bg="#333333")
+
